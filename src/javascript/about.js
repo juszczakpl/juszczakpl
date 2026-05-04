7696b3ddcb75a346618b506643db5e3fcd40a101
Krystian <krystian@juszczak.pl>
2026-05-04 11:01:13 +0200

    Tried JavaScript in Astro website.

---
+++ src/javascript/about.js
@@ -0,0 +1,10 @@
+export const identity = {
+  firstName: "Krystian",
+  country: "Poland",
+  occupation: "Brand Designer",
+  hobbies: ["typography, web development", "photography"],
+}
+
+export const skills = [
+  "HTML", "CSS", "JavaScript", "Astro", "Brand Design"
+]
